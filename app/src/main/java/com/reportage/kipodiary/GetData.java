17d8c05bfd3bb3fbe1eed9d666a58ed59d5e01a0
CursedParadise <mr.nik.kiselev.020202@gmail.com>
2023-05-24 00:10:46 +0300

    Последние доработки на день

--- app/src/main/java/com/reportage/kipodiary/GetData.java
+++ app/src/main/java/com/reportage/kipodiary/GetData.java
@@ -33,15 +33,15 @@ public class GetData extends AsyncTask<Void, Void, String[]> {
 
     public GetData(Context context, TextView tTime, TextView tDiscipline, TextView tAuditorium, TextView tTeacher, int day, String[] maArray, int count, String selectedGroup) {
 
-        this.context= context;
-        this.textTime= tTime;
-        this.textDiscipline= tDiscipline;
-        this.textAuditorium= tAuditorium;
-        this.textTeacher= tTeacher;
-        this.id= day;
+        this.context = context;
+        this.textTime = tTime;
+        this.textDiscipline = tDiscipline;
+        this.textAuditorium = tAuditorium;
+        this.textTeacher = tTeacher;
+        this.id = day;
         this.myArray = maArray;
-        this.lCount=count;
-        this.selectedGroup= selectedGroup;
+        this.lCount = count;
+        this.selectedGroup = selectedGroup;
     }
 
     @Override
@@ -63,41 +63,55 @@ public class GetData extends AsyncTask<Void, Void, String[]> {
             String weekData = firstDayOfWeekStr + " - " + lastDayOfWeekStr;
 
 
+            URL url = new URL("http://mrnikkly.beget.tech/api.php");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
 
-        URL url = new URL("http://mrnikkly.beget.tech/api.php");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setDoInput(true);
+            // Добавляем параметры, если нужно
+            String postData = "id=" + id + "&weekData=" + weekData + "&lCount=" + lCount + "&group=" + selectedGroup;
+            OutputStream os = conn.getOutputStream();
+            os.write(postData.getBytes("UTF-8"));
+            os.flush();
+            os.close();
 
-        // Добавляем параметры, если нужно
-        String postData = "id=" + id + "&weekData=" + weekData + "&lCount=" +lCount + "&group=" +selectedGroup;
-        OutputStream os = conn.getOutputStream();
-        os.write(postData.getBytes("UTF-8"));
-        os.flush();
-        os.close();
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String response = "";
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response += line;
+            }
+            in.close();
 
-        InputStream in = new BufferedInputStream(conn.getInputStream());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String response = "";
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            response += line;
+            // Преобразуем полученную строку в JSON-объект
+            JSONObject jsonObject = new JSONObject(response);
+
+            // Извлекаем данные из объекта JSON
+
+            String lesson_time = jsonObject.getString("Время");
+            String building = jsonObject.getString("Корпус");
+            String auditorium = jsonObject.getString("Аудитория");
+            String discipline = jsonObject.getString("Дисциплина");
+            String teacher = jsonObject.getString("Преподаватель");
+            if (!lesson_time.contains("ПЕРЕЕЗД")) {
+                myArray[0] = lesson_time;
+            }
+            if (!building.contains("ПЕРЕЕЗД ПЕРЕЕЗД")) {
+                myArray[1] = building + " " + auditorium;
+            }
+            if (!discipline.contains("ПЕРЕЕЗД")) {
+                myArray[2] = discipline;
+            }
+            if (!teacher.contains("ПЕРЕЕЗД")) {
+                myArray[3] = teacher;
+            }
+
+            myArray = new String[]{lesson_time, building + " " + auditorium, discipline, teacher};
+
+            conn.disconnect();
         }
-        in.close();
-
-        // Преобразуем полученную строку в JSON-объект
-        JSONObject jsonObject = new JSONObject(response);
-
-        // Извлекаем данные из объекта JSON
-        String lesson_time = jsonObject.getString("Время");
-        String building = jsonObject.getString("Корпус");
-        String auditorium = jsonObject.getString("Аудитория");
-        String discipline = jsonObject.getString("Дисциплина");
-        String teacher = jsonObject.getString("Преподаватель");
-        myArray = new String[]{lesson_time,building + " " + auditorium,discipline,teacher};
-        conn.disconnect();
-    }
 
 
         //При ошибке в try
@@ -105,18 +119,23 @@ public class GetData extends AsyncTask<Void, Void, String[]> {
 
             return new String[]{new String("Exception: " + e.getMessage())};
         }
-return myArray;
+        return myArray;
     }
 
     //Вывод
     @Override
     protected void onPostExecute(String[] result) {
+        //Время
         String time = myArray[0];
         String[] timeParts = time.split("-");
         String formattedTime = "<b>" + timeParts[0] + "</b><br>" + timeParts[1];
         this.textTime.setText(Html.fromHtml(formattedTime));
+
+        //Предмет
         this.textDiscipline.setText(Html.fromHtml(myArray[2]));
+        //Аудитория
         this.textAuditorium.setText(Html.fromHtml(myArray[3]));
+        //Преподаватель
         this.textTeacher.setText(Html.fromHtml(myArray[1]));
     }
 
