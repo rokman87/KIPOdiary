cfdf71783b144c4591d4ce334a6c2aec709cfae5
CursedParadise <mr.nik.kiselev.020202@gmail.com>
2023-05-29 20:58:45 +0300

    Заметки норм просматриваются

--- app/src/main/java/com/reportage/kipodiary/CheckNotes.java
+++ app/src/main/java/com/reportage/kipodiary/CheckNotes.java
@@ -6,18 +6,58 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckNotes extends AppCompatActivity {
+    private TextView notesTextView;
+    private String lessonId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_notes);
 
+        //Получаю id Пары
         Intent intent = getIntent();
-        String lesson_id = intent.getStringExtra("lesson_id");
-        TextView lessonIdTextView = findViewById(R.id.lesson_id_textview);
-        lessonIdTextView.setText(lesson_id);
+        lessonId = intent.getStringExtra("lesson_id");
+        System.out.println("lessonId: " + lessonId);
+        //Задаю значение TextView
+        getNotesFromDataBase();
 
 
+    }
+
+    private void getNotesFromDataBase() {
+        String url = "https://ginkel.ru/kipo/get_notes_from_db.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        notesTextView = findViewById(R.id.lesson_id_textview);
+                        notesTextView.setText(response);// Находим элементы
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Обработка ошибки
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("lessonId", lessonId);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(stringRequest);
     }
 }
